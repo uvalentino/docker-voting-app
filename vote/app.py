dc093a668882a31daa5f2326249cdad4d4ec1a85
Ubeujum Valentino <ubeujumv@gmail.com>
2024-12-10 17:33:04 +0100

    edit

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Rain")
+option_b = os.getenv('OPTION_B', "Snow")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
